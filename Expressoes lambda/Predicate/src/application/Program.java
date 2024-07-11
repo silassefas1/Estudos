dfac129801408cf9ecda770f31b39fd035f50365
silas sefas <silassefas23@gmail.com>
2024-07-11 08:52:04 -0300

    First way to remove elements from list

--- Expressoes lambda/Predicate/src/application/Program.java
+++ Expressoes lambda/Predicate/src/application/Program.java
@@ -16,6 +16,13 @@ public class Program {
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("HD Case", 80.90));
 		
+		/*Uma forma de remover todos os elementos da lista com preço maior que $100*/
+		list.removeIf(p -> p.getPrice() >= 100);
+		
+		
+		for(Product p: list) {
+			System.out.println(p);
+		}
 	}
 
 }
