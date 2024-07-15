d85d09cb9d50c8941b0a03273c8508c5c8eed52d
silas sefas <silassefas23@gmail.com>
2024-07-15 11:12:25 -0300

    Ordenando os produtos com preço abaixo da media em ordem decrescente

--- Expressoes lambda/ExercicioResolvido/src/application/Program.java
+++ Expressoes lambda/ExercicioResolvido/src/application/Program.java
@@ -4,9 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import entities.Product;
 
@@ -32,9 +34,16 @@ public class Program {
 			double average = list.stream() //criando a stream da lista
 					.map(p -> p.getPrice()) // criando um map da lista so com o preço dos produtos
 					.reduce(0.0, (x,y) -> x+y) /list.size(); //usando a função reduce com um par para somar os elemntos da lista
-			
 			System.out.println("Average price: " + String.format("%.2f", average));
 			
+			Comparator<String> comp = (s1,s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+			
+			List<String> names = list.stream()
+					.filter(p -> p.getPrice() < average)
+					.map(p -> p.getNome())
+					.sorted(comp.reversed())
+					.collect(Collectors.toList());
+			names.forEach(System.out::println);
 			
 		}
 		catch(IOException e) {
