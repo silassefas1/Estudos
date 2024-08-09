f3662daf379c7f04d1aaf9746e7cdfeaea1e29bf
Silas Sefas <silassefas23@gmail.com>
2024-08-09 16:55:45 -0300

    Update with Translations

--- CursoMySQL-JDBC/Translations/src/application/Program.java
+++ CursoMySQL-JDBC/Translations/src/application/Program.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import db.DB;
+import db.DbException;
 
 public class Program {
 
@@ -17,20 +18,31 @@ public class Program {
 		try {
 			connection = DB.getConnection();
 			statement = connection.createStatement();
-			int affectedRows = statement.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
 			
+			connection.setAutoCommit(false); // aqui desativa o alto commit das operações
+			
+			int affectedRows = statement.executeUpdate("UPDATE seller SET BaseSalary = 3010 WHERE DepartmentId = 1");
+			/*
 			// Teste de error na trasação
 			int x=1;
 			if(x<2) {
 				throw new SQLException("Fake error");
-			}
-			int affectedRows2 = statement.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+			}*/
+			int affectedRows2 = statement.executeUpdate("UPDATE seller SET BaseSalary = 4015 WHERE DepartmentId = 2");
+			
+			connection.commit(); // passa o update do bloco inteiro, se der erro no caminho não atualiza.
 			
 			System.out.println("AffectedRows 1: " + affectedRows);
 			System.out.println("AffectedRows 2: " + affectedRows2);
 		}
 		catch(SQLException e) {
-			e.printStackTrace();
+			try {
+				connection.rollback();// volta o banco ao ultimo estado de integridade
+				throw new DbException("Transaction rolled back! Caused by: "+ e.getMessage());
+			} catch (SQLException e1) {
+				//Erro no rollback
+				throw new DbException("Erro trying to rollback! Caused by:  "+ e1.getMessage());
+			} 
 		}
 		finally {
 			DB.closeStatement(statement);
