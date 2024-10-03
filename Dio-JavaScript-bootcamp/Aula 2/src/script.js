c6559ce41eb4529006ee738ac4a31e933458791f
Silas Sefas <silassefas23@gmail.com>
2024-10-03 09:33:19 -0300

    tipos de variaveis

---
+++ Dio-JavaScript-bootcamp/Aula 2/src/script.js
@@ -0,0 +1,17 @@
+//tipos de variaveis
+
+//pokemon
+let nomePokemon = "Pikachu" //String
+let nivelPokemon = 20 // Number
+let pontosDeVidaPokemon = 45 // Number
+let sexoDoPokemon = "M" // String
+let pokemonSelecionavel = true //Boolean
+
+//exercico de fixação, salvar os dados de uma pessoa em variaveis
+//nome, idade, numero de telefone, cpf e endereço 
+
+let nomePessoa = "Josecleide Maria de silva sa santo moura"
+let idade = 78
+let numetoTelefone = "87 9654321221"
+let cpf = "123.455.461-87"
+let enderecoPessoa = "Rua das Floeres n 10"
