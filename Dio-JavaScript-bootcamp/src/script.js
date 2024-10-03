f754c60ad5b720166a2dbfb8f74b344e01ec3f8a
Silas Sefas <silassefas23@gmail.com>
2024-10-03 08:38:42 -0300

    Constantes JavaScript

--- Dio-JavaScript-bootcamp/src/script.js
+++ Dio-JavaScript-bootcamp/src/script.js
@@ -1,5 +1,12 @@
-console.log("Digite seu nome de jogador: ")
+console.log('Digite seu nome de jogador: ');
 
-let nickname  = "Silas PokeMaster"
+let nickname = 'Silas PokeMaster';
 
-console.log("Bem vindo " + nickname)
+console.log('Bem vindo ' + nickname);
+console.log(nickname + 'entrou no servidor!');
+
+//constantes não podem ser alteradas.
+const notificacao = 'Pokemon go diz: ';
+
+console.log(notificacao + 'tem um novo pokemon na sua região.');
+console.log(notificacao + 'você foi derotado.');
