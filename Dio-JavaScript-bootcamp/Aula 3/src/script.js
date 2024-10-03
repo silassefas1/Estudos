20a8df36b49ba430e490c0f58bc33dd94a55cc84
Silas Sefas <silassefas23@gmail.com>
2024-10-03 11:40:46 -0300

    Vetores em JavaScript

---
+++ Dio-JavaScript-bootcamp/Aula 3/src/script.js
@@ -0,0 +1,15 @@
+//Vetor
+let pokemons = ["Pikachu", "Charmander", "Bulbassauro"]
+console.log(pokemons)
+console.log(pokemons[2])
+pokemons.pop()
+
+console.log(pokemons)
+pokemons.shift()
+console.log(pokemons)
+
+
+
+
+
+
