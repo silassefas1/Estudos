bfc5e9d7f5859ff8231465b5ec1c3fff41bc1fad
Silas Sefas <silassefas23@gmail.com>
2024-10-04 08:24:50 -0300

    Vetores e Matrizes

--- Dio-JavaScript-bootcamp/Aula 3/src/script.js
+++ Dio-JavaScript-bootcamp/Aula 3/src/script.js
@@ -1,15 +1,19 @@
 //Vetor
+console.log("Vetores")
 let pokemons = ["Pikachu", "Charmander", "Bulbassauro"]
 console.log(pokemons)
 console.log(pokemons[2])
-pokemons.pop()
 
 console.log(pokemons)
-pokemons.shift()
-console.log(pokemons)
+console.log(pokemons.length)
 
-
-
-
-
+//Matrizes
+console.log("Matrizes")
+let timePokemon = [
+   ["Pikacu", "M", "Level 1",],
+   ["Charmander", "F", "Level 4",]
+]
+console.log(timePokemon)
+console.log("O pokemon " + timePokemon[1][0] + "é do sexo "
+ + timePokemon[1][1] + "e esta no level " + timePokemon[1][2])
 
