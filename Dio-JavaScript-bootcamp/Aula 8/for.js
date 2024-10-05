d2a68f63feb2e5cf86405e2476493409c788b58b
Silas Sefas <silassefas23@gmail.com>
2024-10-05 08:26:35 -0300

    For, While e do-while

---
+++ Dio-JavaScript-bootcamp/Aula 8/for.js
@@ -0,0 +1,12 @@
+// for
+for (let contador = 0; contador < 4; contador++){
+    console.log(contador)
+}
+
+let pontosDeVida = 0;
+for(let interador = 0; interador< 10; interador++){
+    pontosDeVida += 1
+    console.log("Tomou uma poça magica! " + interador)
+}
+
+console.log("Ponto de vidas Totais: " + pontosDeVida)
---
+++ Dio-JavaScript-bootcamp/Aula 8/while.js
@@ -0,0 +1,11 @@
+//while
+let contador = 0
+while(contador < 3){
+    console.log(contador);
+    contador++
+}
+contador =0
+do {
+    console.log("Ola")
+    contador++
+}while(contador <= 2);
